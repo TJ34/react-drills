91ae45dbd2c57c4a3cc5b7c5820669f772889c65
Anthony Romero <anthonyromero@Anthonys-MacBook-Pro.local>
2018-09-08 09:50:01 -0500

    first 2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      random: ['Spectrasoul','Technimatic','Etherwood','LSB','High Contrast']
+    }
+  }
+
+
   render() {
+    const artists = this.state.random.map((artist,i) => {
+      return (<h2 key={i}>{artist}</h2>)
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {artists}
       </div>
     );
   }
