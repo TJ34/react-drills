9176588658edc25748c2903d2a7a172d240f331f
Anthony Romero <anthonyromero@Anthonys-MacBook-Pro.local>
2018-09-09 14:17:59 -0500

    question 5 and 6 completed

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Image from './Image';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image img={"https://images.unsplash.com/photo-1536367340461-06b49ffdf5b3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=363680b137af427e7d80adf34b7c38cc&auto=format&fit=crop&w=800&q=80"}/>
       </div>
     );
   }
--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: '',
+      list: []
+    }
+
+    this.addToList = this.addToList.bind(this)
+
+  }
+
+  taskChange = (val) => {
+    this.setState({task: val})
+  }
+
+  addToList = () => {
+    this.setState({ list: [...this.state.list, this.state.task], task: ''})
+  }
+
   render() {
+
+    let list = this.state.list.map((element, i)=>{
+      return (
+        <Todo key={i} task={element}/>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <p className="heading">My to-do list:</p>
+
+        <div>
+          <input value={this.state.task} onChange={(e) => this.taskChange(e.target.value)} placeholder="Enter new task"/>
+          <button onClick={this.addToList}>Add</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {list}
       </div>
     );
   }
